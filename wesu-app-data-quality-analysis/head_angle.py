3b0a8ffa922619ea25da46ec074c28259c49e895
SB <sbrandys13@gmail.com>
2018-01-04 17:16:43 +0100

    Corrected acc vector for more accurate sensor orientation + cut data to display meaningful head gyro sample

--- wesu-app-data-quality-analysis/head_angle.py
+++ wesu-app-data-quality-analysis/head_angle.py
@@ -28,14 +28,39 @@ def plotZAxis2(dataFrame1, dataFrame2, label1 = "DF1", label2 = "DF2"):
 
 
 def run():
-    cmd_arg_accelerationVector = pd.np.array([-773, 612, 175])
+    # cmd_arg_accelerationVector = pd.np.array([-773, 612, 175])
+    cmd_arg_accelerationVector = pd.np.array([-1000, -125, 170])
 
     quat = qt_from_two_vectors(cmd_arg_accelerationVector, z_axis_vector)
     head_gyr_data = load_data_of(dev_head, cat_gyroscope)
     head_gyr_orient_quaternion_data = orientate_data_using_quaternion(head_gyr_data, quat)
 
-    interpolatedData = interpolate_data(head_gyr_orient_quaternion_data, 10)
-    plotZAxis2(head_gyr_orient_quaternion_data.df, interpolatedData.df, "Non interpolated", "Interpolated")
+    # correctData = head_gyr_orient_quaternion_data.df[head_gyr_orient_quaternion_data.df[file_header_hostTimestamp] > 530000]
+
+    # data in between 17:07:05 - 17:07:15 - 1 full head turn
+    correctData = head_gyr_orient_quaternion_data.df[(head_gyr_orient_quaternion_data.df[file_header_hostTimestamp] > 550000) & (head_gyr_orient_quaternion_data.df[file_header_hostTimestamp] < 568000)]
+    print("All data size: %d, correct data size: %d" % (len(head_gyr_orient_quaternion_data.df.index), len(correctData.index)))
+
+    interpolatedData = interpolate_data(Data(head_gyr_orient_quaternion_data.device, head_gyr_orient_quaternion_data.category, correctData), 10)
+    # interpolatedData = Data(head_gyr_orient_quaternion_data.device, head_gyr_orient_quaternion_data.category, correctData)
+    plotZAxis2(correctData, interpolatedData.df, "Non interpolated", "Interpolated")
+
+    zDps = np.array(interpolatedData.df[file_header_Z_dps])
+    print(zDps)
+    print("Negative zDPS sum: %d" % sum(zDps[zDps < 0]))
+    print("Non negative zDPS sum: %d" % sum(zDps[zDps >= 0]))
+
+
+    z = interpolatedData.df[file_header_Z_dps]
+    t = interpolatedData.df[file_header_hostTimestamp]
+    z_cum_int = integrate.cumtrapz(z, t / 1000, initial=0)
+    fig, ax = plt.subplots()
+    fig.set_size_inches(24.4, 6)
+    plt.title("Cumulative trapez integration")
+    # ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%H:%M:%S"))
+    plt.grid()
+    plt.plot(t, z_cum_int, marker='.', linestyle='--')
+    plt.show()
 
 
 if __name__ == "__main__":
